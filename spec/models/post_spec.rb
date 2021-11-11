154443d5c4d16e582992f19784ee6fbde8456858
Dean Abbott <deanabbott293@gmail.com>
2021-11-11 13:06:42 +0900

    setting post title test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,16 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   current_user = User.find_or_create!(email: 'dean@example.com', password: 'password', password_confirmation: 'password')
   it 'has a title' do
+    post = Post.new(
+      title: '',
+      body: 'A valid body',
+      user: current_user,
+      views: 0
+    )
+    expect(post).to_not be_valid
 
+    post.title = 'A new valid title'
+    expect(post).to be_valid
   end
 
   it 'has a body' do
