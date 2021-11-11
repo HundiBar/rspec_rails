452475a56575fda4296fe666c374e5e368069ad3
Dean Abbott <deanabbott293@gmail.com>
2021-11-11 13:05:06 +0900

    setting tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  current_user = User.find_or_create!(email: 'dean@example.com', password: 'password', password_confirmation: 'password')
+  it 'has a title' do
+
+  end
+
+  it 'has a body' do
+
+  end
+
+  it 'has a title at least 2 chars long' do
+
+  end
+
+  it 'has a body between 5 and 100 chars' do
+
+  end
+
+  it 'has numerical views' do
+
+  end
 end
