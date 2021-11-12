5ce99b6eac8213609b72956b3789fff8c2a6e674
Dean Abbott <deanabbott293@gmail.com>
2021-11-12 10:40:30 +0900

    user test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  current_user = User.first_or_create!(email: 'dean@example.com', password: 'password', password_confirmation: 'password')
+
+  it 'has an id' do
+    expect(current_user.id).to eq(1)
+  end
 end
